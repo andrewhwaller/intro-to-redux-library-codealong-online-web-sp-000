c52ffc6e92e590a033cd8b8872ab2bac141c64d5
Andrew Waller <andrewhwaller@gmail.com>
2020-02-19 02:56:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -13,5 +13,4 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(<App store={store} />, document.getElementById('root')
-);
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
